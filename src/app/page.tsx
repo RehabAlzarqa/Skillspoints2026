cbeea8d2428d511c5b0d61cf5cd8952b45231480
rehab alzarqa <essamrehab98@gmail.com>
2026-09-17 09:30:14 +0200

    UI start dev

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+
+const page = () => {
+    return ( 
+        <div className="w-full h-screen bg-yellow-700">
+            <Header />
+        </div>
+     );
+}
+ 
+export default page;
